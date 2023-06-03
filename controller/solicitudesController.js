4fc140f7cd8eb771f1d706a750b54fad0db680f0
FedericoRohr <rohrfede@gmail.com>
2023-06-03 18:33:27 -0300

    se agrego metodo para eliminar solicitud

--- controller/solicitudesController.js
+++ controller/solicitudesController.js
@@ -12,8 +12,13 @@ class ControladorSolicitudes {
     }
     
     getSolicitudes = async (req,res) => {
-        const { id } = req.params
-        res.json(await this.apiSolicitudes.obtenerSolicitudes(id))
+        const { idSolicitud } = req.params
+        res.json(await this.apiSolicitudes.obtenerSolicitudes(idSolicitud))
+    }
+
+    deleteSolicitud = async (req,res) =>{
+        const { idSolicitud } = req.params
+        res.json(await this.apiSolicitudes.eliminarSolicitud(idSolicitud))
     }
 }
 
