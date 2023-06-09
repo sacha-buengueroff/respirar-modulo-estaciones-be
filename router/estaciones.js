65427fde33156e1eee692cc371495088b3e103a6
FedericoRohr <rohrfede@gmail.com>
2023-06-09 12:30:59 -0300

    Se elimina del router getDatosConexion y PutDatosConexion

--- router/estaciones.js
+++ router/estaciones.js
@@ -32,18 +32,7 @@ export class RouterEstaciones {
         */
         )
 
-        /* GET Visualización de Datos de Conexión de Estación */
-        this.router.get('/datosConexion/:id', this.controladorEstaciones.getDatosConexion
-            /*
-            #swagger.tags = ['Estaciones']
-            #swagger.description = 'Endpoint para visualizar datos de conexión de estación.'
-            #swagger.parameters["id"] = {
-                in: "path",
-                required: "true"
-            }
-            */
-        )
-
+        
         /* PUT Suspender Estación */
         this.router.put('/suspenderEstacion/:id', this.controladorEstaciones.suspenderEstacion
             /*
@@ -61,18 +50,6 @@ export class RouterEstaciones {
             /*
             #swagger.tags = ['Estaciones']
             #swagger.description = 'Endpoint para habilitar una estación.'
-            #swagger.parameters["id"] = {
-                in: "path",
-                required: "true"
-            }
-            */
-        )
-
-        /* PUT Modificación de Datos de Conexión de Estación */
-        this.router.put('/datosConexion/:id', this.controladorEstaciones.putDatosConexion
-            /*
-            #swagger.tags = ['Estaciones']
-            #swagger.description = 'Endpoint para modificar datos de conexión de estación.'
             #swagger.parameters["id"] = {
                 in: "path",
                 required: "true"
