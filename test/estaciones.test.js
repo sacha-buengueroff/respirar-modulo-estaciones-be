27dd0d47e81ff5b84ff1fc0bd9fb1fc5874b2b24
sacha-buengueroff <sachabuengue@gmail.com>
2023-06-17 20:59:33 -0300

    pruebas hechas

--- test/estaciones.test.js
+++ test/estaciones.test.js
@@ -6,8 +6,6 @@ import config from "../config.js";
 
 import axios from "axios";
 
-console.log("Prueba")
-
 describe("test endpoints estaciones", () => {
     let server;
     let app;
@@ -83,6 +81,36 @@ describe("test endpoints estaciones", () => {
             });
 
         })
+
+        
+    })
+
+    describe("GET estaciones", async () => {
+        let id_iot
+        let id_cb
+
+        before(async () => {
+            let body = {
+                name: "Obelisco",
+                coordinates: [
+                    "111",
+                    "222"
+                ],
+                addStreet: "Av. 9 de Julio",
+                addLocaly: "San Nicolas",
+                addRegion: "CABA",
+                external: false
+            }
+            let response = await request.post("/estaciones/").send(body)
+            id_iot = response.body.mailId
+            id_cb = response.body.id
+        })
+
+        after(async () => {
+            await axios.delete(`${urlCb}${id_cb}`, req_config)
+            await axios.delete(`${urlIot}${id_iot}`, req_config)
+        })
+
         it("Debería retornar la estacion especifica cuando se pasa id", async () => {
             let response = await request.get(`/estaciones/${id_cb}`)
             expect(response.status).to.eql(200)
@@ -104,5 +132,126 @@ describe("test endpoints estaciones", () => {
                 "temperature"
             );
         })
+
+        it("Debería retornar todas las estaciones si no se envía el id", async () => {
+            let response = await request.get("/estaciones")
+            expect(response.status).to.eql(200)
+
+            let estaciones = response.body
+            estaciones.forEach(estacion => {
+                expect(estacion).to.include.keys(
+                    "id",
+                    "type",
+                    "TimeInstant",
+                    "address",
+                    "dataProvider",
+                    "enable",
+                    "location",
+                    "ownerId",
+                    "pm1",
+                    "pm10",
+                    "pm25",
+                    "reliability",
+                    "temperature"
+                )
+            })
+        })
+
+        it("Debería retornar error si se pasa un id invalido", async () => {
+            let id_invalido = "aaaaaa"
+            let response = await request.get(`/estaciones/${id_invalido}`)
+
+            expect(response.status).to.eql(404)
+            expect(response.body).to.eql("The requested entity has not been found. Check type and id")
+        })
+    })
+
+    describe("PUT suspender/habilitar estación", async () => {
+        let id_iot
+        let id_cb
+
+        before(async () => {
+            let body = {
+                name: "Obelisco",
+                coordinates: [
+                    "111",
+                    "222"
+                ],
+                addStreet: "Av. 9 de Julio",
+                addLocaly: "San Nicolas",
+                addRegion: "CABA",
+                external: false
+            }
+            let response = await request.post("/estaciones/").send(body)
+            id_iot = response.body.mailId
+            id_cb = response.body.id
+        })
+
+        after(async () => {
+            await axios.delete(`${urlCb}${id_cb}`, req_config)
+            await axios.delete(`${urlIot}${id_iot}`, req_config)
+        })
+
+        it("Debería suspender la estación habilitada", async () => {
+            let response = await request.put(`/estaciones/suspenderEstacion/${id_cb}`)
+            expect(response.status).to.eql(204)
+        })
+
+        it("Debería arrojar error al suspender la estación suspendida", async () => {
+            let response = await request.put(`/estaciones/suspenderEstacion/${id_cb}`)
+            expect(response.status).to.eql(404)
+            expect(response.body).to.eql(`Error al suspender el dispositivo ${id_iot}`)
+        })
+
+        it("Debería habilitar la estación suspendida", async () => {
+            let response = await request.put(`/estaciones/habilitarEstacion/${id_cb}`)
+            expect(response.status).to.eql(201)
+            expect(response.body).to.eql(`Se habilito correctamente el dispositivo ${id_cb}`)
+        })
+
+        it("Debería arrojar error al habilitar la estación habilitada", async () => {
+            let response = await request.put(`/estaciones/habilitarEstacion/${id_cb}`)
+            expect(response.status).to.eql(409)
+            expect(response.body).to.eql(`Error al habilitar el dispositivo ${id_cb}`)
+        })
+    })
+
+    describe("POST datos de estación", async () => {
+        let id_iot
+        let id_cb
+        let key = "4jggokgpepnvsb2uv4s40d59ov"
+        let body_data = "r|1|t|22|pm1|10|pm10|5|pm25|5"
+
+        before(async () => {
+            let body = {
+                name: "Obelisco",
+                coordinates: [
+                    "111",
+                    "222"
+                ],
+                addStreet: "Av. 9 de Julio",
+                addLocaly: "San Nicolas",
+                addRegion: "CABA",
+                external: false
+            }
+            let response = await request.post("/estaciones/").send(body)
+            id_iot = response.body.mailId
+            id_cb = response.body.id
+        })
+
+        after(async () => {
+            await axios.delete(`${urlCb}${id_cb}`, req_config)
+            await axios.delete(`${urlIot}${id_iot}`, req_config)
+        })
+
+        it("Debería enviar los datos al dispositivo", async () => {
+            let response = await request.post(`/estaciones/data/?k=${key}&i=${id_iot}`)
+            .set({
+                'Content-Type': 'text/plain'
+            }).send(body_data)
+            expect(response.status).to.eql(200)
+            expect(response.body).to.eql("Se cargaron correctamente los datos")
+        })
+
     })
 })
