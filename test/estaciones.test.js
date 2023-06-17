d5dfd926dbf50d0d3217a1750db4b8f18b82d42a
sacha-buengueroff <sachabuengue@gmail.com>
2023-06-17 11:53:17 -0300

    estaciones agregado

---
+++ test/estaciones.test.js
@@ -0,0 +1,9 @@
+import supertest from "supertest";
+import { expect } from "chai";
+
+import Server from "../server.js";
+import config from "../config.js";
+
+describe("test endpoints estaciones", () => {
+    
+})
