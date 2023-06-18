47442d0a9c94e0c3c7b28f734eafa8dfddb6a12d
FedericoRohr <rohrfede@gmail.com>
2023-06-18 18:09:16 -0300

    Se agrega schema de estacion

--- swagger.js
+++ swagger.js
@@ -6,6 +6,68 @@ const doc = {
     },
     host: 'localhost:8080',
     schemes: ['http'],
+    definitions: {
+      Estacion: {
+        "id": "urn:ngsi-ld:AirQualityObserved:1",
+        "type": "AirQualityObserved",
+        "TimeInstant": {
+            "type": "DateTime",
+            "value": "2023-06-17T14:47:05.634Z",
+        },
+        "address": {
+            "type": "Address",
+            "value": {
+                "address": {
+                    "streetAddress": "corrientes",
+                    "addressLocality": "CABA",
+                    "addressRegion": "PAMPA"
+                }
+            },
+        },
+        "dataProvider": {
+            "type": "String",
+            "value": "Respirar",
+        },
+        "enable": {
+            "type": "Boolean",
+            "value": true,
+        },
+        "location": {
+            "type": "Point",
+            "value": {
+                "coordinates": [
+                    -38.6486495,
+                    -163.369692
+                ]
+            },
+        },
+        "ownerId": {
+            "type": "String",
+            "value": "urn:ngsi-ld:name",
+        },
+        "pm1": {
+            "type": "Float",
+            "value": null,
+        },
+        "pm10": {
+            "type": "Float",
+            "value": null,
+        },
+        "pm25": {
+            "type": "Float",
+            "value": null,
+        },
+        "reliability": {
+            "type": "Float",
+            "value": null,
+        },
+        "temperature": {
+            "type": "Float",
+            "value": null,
+        }
+    },
+    }
+
   };
 
 const outputFile = './swagger_output.json'
